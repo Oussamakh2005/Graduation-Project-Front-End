0e30450f6ec13783dd09dc8abd237ae6b92c75e7
Oussama <khelefoussama2005@gmail.com>
2025-04-30 20:25:26 +0100

    fix api url

--- js/auth/isClient.js
+++ js/auth/isClient.js
@@ -1,4 +1,4 @@
 import { authorized } from "./authorized.js";
 import { config } from "../utils/config.js";
 
-authorized(`${config.API_URL}/api/user/client`);
+authorized(`${config.API_URL}/api/user/auhtenticated`);
