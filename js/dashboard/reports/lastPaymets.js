62ab462cdfbbb7dcacaf19be69d3a94c894dbdd4
Oussama <khelefoussama2005@gmail.com>
2025-05-04 22:42:31 +0100

    connect payment dashboard hightlihgts and last payments with the backend api

---
+++ js/dashboard/reports/lastPaymets.js
@@ -0,0 +1,39 @@
+import { config } from "../../utils/config.js";
+import { showToast } from "../../utils/showToast.js";
+document.addEventListener("DOMContentLoaded", async () => {
+    const paymentsTable = document.getElementById("payments-table");
+    const token = localStorage.getItem("token");
+    try {
+        const response = await fetch(`${config.API_URL}/api/report/lastPayments`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `${token}`,
+            }
+        });
+        const data = await response.json();
+        if (data.ok) {
+            const payments = data.data;
+            for(const payment of payments){
+                paymentsTable.innerHTML += `
+                <tr>
+                   <td>${payment.id}</td>
+                   <td>${payment.sale.user.firstName + " "+payment.sale.user.lastName}</td>
+                   <td>${payment.paymentAmount + " دينار"}</td>
+                   <td>${setPaymentType(payment.paymentType)}</td>
+                   <td>${payment.paymentDate}</td>
+                </tr>
+                `;
+            }
+        } else {
+            showToast("فشل الحصول على البيانات يرجى إعادة المحاولة", "error")
+        }
+    } catch (error) {
+        console.log(error)
+        showToast("حدث خطأ غير متوقع يرجى إعادة المحاولة لاحقا", "error");
+    }
+
+});
+
+function setPaymentType(type){
+    return (type == 'FULL_PAYMENT')? `<span class="status completed"> تكميلية</span>` : `<span class="status pending">مسبقه</span>`;
+}
---
+++ js/dashboard/reports/paymentHighlights.js
@@ -0,0 +1,26 @@
+import { config } from "../../utils/config.js";
+import { showToast } from "../../utils/showToast.js";
+document.addEventListener('DOMContentLoaded', async () => {
+    const fullPaymentsCard = document.getElementById("full-payments");
+    const downPaymentsCard = document.getElementById("down-payments");
+    const totalRevenuCard = document.getElementById("total-revenues");
+    const token = localStorage.getItem('token');
+    try{
+        const response = await fetch(`${config.API_URL}/api/report/paymentHighlights`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `${token}`,
+            }
+        });
+        const data = await response.json();
+        if(data.ok){
+            fullPaymentsCard.innerText = data.data.fullPayments;
+            downPaymentsCard.innerText = data.data.downPayments;
+            totalRevenuCard.innerText = data.data.totalRevenu + " دينار"; 
+        }else{
+            showToast("فشل الحصول على البيانات يرجى إعادة المحاولة","error")
+        }
+    }catch(error){
+        showToast("حدث خطأ غير متوقع يرجى إعادة المحاولة لاحقا", "error");
+    }
+});
