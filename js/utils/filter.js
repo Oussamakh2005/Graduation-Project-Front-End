d69a31b07669ee4b91a502b8c1cac96eeafebff8
Oussama <khelefoussama2005@gmail.com>
2025-05-25 05:12:48 +0100

    fix car filter

--- js/utils/filter.js
+++ js/utils/filter.js
@@ -1,17 +1,17 @@
 let displayedCards = 0;
 function applyFilter() {
-  const price = document.getElementById("price").value;
+  /*const price = document.getElementById("price").value;*/
   const fuel = document.getElementById("fuel").value;
   const transmission = document.getElementById("transmission").value;
   const seats = document.getElementById("seats").value;
   displayedCards = 0;
   document.querySelectorAll(".car-card").forEach(card => {
-    const matchPrice = !price || card.dataset.price === price;
+    /*const matchPrice = !price || card.dataset.price === price;*/
     const matchFuel = !fuel || card.dataset.fuel === fuel;
     const matchTrans = !transmission || card.dataset.transmission === transmission;
     const matchSeats = !seats || card.dataset.seats === seats;
 
-    card.style.display = (matchPrice && matchFuel && matchTrans && matchSeats) ? displayedCarsIncrease() :"none";
+    card.style.display = (/*matchPrice &&*/ matchFuel && matchTrans && matchSeats) ? displayedCarsIncrease() :"none";
   });
   const carList = document.getElementById("car-list");
   if(displayedCards === 0){
